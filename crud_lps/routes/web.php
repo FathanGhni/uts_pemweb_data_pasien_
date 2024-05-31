5ea5e5e89386daa15269e80a39f31203b6f746e9
Athan <157662250+FathanGhni@users.noreply.github.com>
2024-06-01 06:01:36 +0700

    Update web.php

--- crud_lps/routes/web.php
+++ crud_lps/routes/web.php
@@ -2,21 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PasienController;
-use App\Http\Controllers\ProductController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// //route resource for products
-// Route::resource('/products', \App\Http\Controllers\ProductController::class);
-
-// Route::get('/', [ProductController::class, 'index']);
-
+//route get for pasiens
 Route::get('/', [PasienController::class, 'index']);
-
-// //route resource for products
-// Route::resource('products', ProductController::class);
 
 //route resource for pasiens
 Route::resource('pasiens', PasienController::class);
